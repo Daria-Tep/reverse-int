ffb57fcd207631771205bf59452db6bdc3b2c427
daria-tep <donum08@gmail.com>
2022-01-04 12:00:57 +0700

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) { n = -n };
+    let arr = n.toString().split("");
+    let res = [];
+
+
+
+    for (let i = 0; i < arr.length; i++) {
+        res.unshift(arr[i])
+    }
+
+
+    return +(res.join(""))
+
 }
